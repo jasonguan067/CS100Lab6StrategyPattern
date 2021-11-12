789df23e067958cb1d90255da079149782a01bd2
jasonguan067 <jasonguan067@gmail.com>
2021-11-11 18:42:36 -0800

    finished final implementation in select.hpp

--- select.hpp
+++ select.hpp
@@ -61,6 +61,8 @@ public:
 	    }
     }	 
 
+
+
 };
 
 class Select_And : public Select {
@@ -98,6 +100,45 @@ public:
     virtual bool select(const Spreadsheet* sheet, int row) const {
         return !(a->select(sheet, row));
     }
+
+};
+
+
+class Select_Or : public Select{
+	protected:
+	  Select* temp1;
+	  Select* temp2;
+
+	public:
+	  Select_Or(Select* left, Select* right){
+		temp1 = left;
+		temp2 = right;
+	
+	}
+
+	~Select_Or(){
+	delete temp1;
+	delete temp2;
+	}
+
+	virtual bool select(const Spreadsheet* sheet, int row) const{
+	   bool flag1 = false;
+	   bool flag2 = false;
+
+	   flag1 = temp1->select(sheet,row);
+	   flag2 = temp2->select(sheet,row);
+
+	   if(flag1 == true || flag2 == true){
+		return true;
+	} else {
+		return false;
+
+	}
+
+
+
+	}
+
 
 };
 
